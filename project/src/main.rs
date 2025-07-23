14163d6272a667ae12598d83eb799a0b108a5653
pete <56932526+putyforce@users.noreply.github.com>
2025-07-23 12:43:57 +0500

    work with vars
    no games only study hard

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,35 +1,17 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
+    let x = 5;
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let x = x + 1;
 
-    loop {
-        println!("Please input your guess.");
+    const Y:i32 = 69;
 
-        let mut guess = String::new();
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+        println!("Does inner works like gate? Yay, Y now is: {Y}");
     }
+
+    println!("The value of x is: {x}");
+    println!("The const Y is: {Y}");
 }
