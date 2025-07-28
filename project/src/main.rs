4a2995a56d2bb8818f6bdb689f4f6f5f442aeffd
pete <56932526+putyforce@users.noreply.github.com>
2025-07-28 13:59:03 +0500

    another return
    here i made a function that increments x for 1 and call it in main as return value

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,9 +1,9 @@
-fn five() -> i32 {
-    22
+fn main() {
+    let x = plus_one(44);
+
+    println!("The value of x is: {x}");
 }
 
-fn main() {
-    let x = five();
-
-    println!("smaller protein scope is: {x}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
